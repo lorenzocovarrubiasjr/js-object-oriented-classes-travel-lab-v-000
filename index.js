895fc089df53a976f3da7fe2b0943e4bcbd3164a
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-16 04:42:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+class Driver {
+  constructor(name, string_date){
+    this.name = name;
+    this.string_date = string_date;
+  }
 
+  startDate() {
+    let start_date = new Date(this.string_date);
+    return start_date;
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+      return year - Date.prototype.getFullYear(this.startDate());
+  }
+
+}
+
+/* class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let total = 0;
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+      ];
+
+    total += Math.abs(beginningLocation[vertical] - endingLocation[vertical]);
+    total += Math.abs((eastWest.indexOf(beginningLocation[horizontal]) + 1) - (eastWest.indexOf(beginningLocation[horizontal]) + 1));
+
+    return total;
+  }
+
+  estimatedTime(peak = false){
+    if (peak === true) {
+      return blocksTravelled() % 3
+    }
+    else {
+      return blocksTravelled() % 2
+    }
+  }
+
+}
+*/
